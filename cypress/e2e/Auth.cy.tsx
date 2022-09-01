1572f74cb15659b35da4c7fcd1f56ee3f851c45f
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-09-01 20:38:54 +0300

    Auth tests: rename username to email

--- cypress/e2e/Auth.cy.tsx
+++ cypress/e2e/Auth.cy.tsx
@@ -2,30 +2,34 @@ import cypress from "cypress";
 import React from "react";
 
 describe("Auth", () => {
+	beforeEach(() => {
+		cy.wait(500);
+	});
+
 	it("Login as test user", () => {
 		cy.visit("/");
-		const username = "test@mail.com";
+		const email = "test@mail.com";
 		const password = "BAJFr2FF";
 
-		cy.get("input#email").type(username);
+		cy.get("input#email").type(email);
 		cy.get("input#password").type(`${password}{enter}`);
 
 		cy.url().should("include", "/");
-		cy.contains(username);
+		cy.contains(email);
 	});
 
 	it("Register a new user", () => {
 		cy.visit("/");
 		const rand = (Math.random() * 100).toFixed(0);
-		const username = `user${rand}@mail.com`;
+		const email = `user${rand}@mail.com`;
 		const password = "password1";
 
 		cy.get("button").contains("Sign Up").as("signUpButton").click();
-		cy.get("input#email").type(username);
+		cy.get("input#email").type(email);
 		cy.get("input#password").type(`${password}`);
 		cy.get("input#confirm_password").type(`${password}{enter}`);
 
 		cy.url().should("include", "/");
-		cy.contains(username);
+		cy.contains(email);
 	});
 });
